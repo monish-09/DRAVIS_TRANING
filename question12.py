ce03615b2d98c5fc67595d8b5368283003cb022a
monish-09 <mkha22647@gmail.com>
2026-03-18 11:23:53 +0530

    Delete question12.py

--- question12.py
+++
@@ -1,8 +0,0 @@
-# Question12 check whether a person is eligible to vote
-age = int(input("Enter your age: "))
-if (age<0):
-    print("Age are not negative")
-elif (age >= 18):
-    print("You are eligible to vote.")
-else:
-    print("You are not eligible to vote.")
